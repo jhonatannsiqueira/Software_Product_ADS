364af6301247e70ca914f6a1b7367aa83179ab1f
Jhonatan <john-nunes@outlook.com>
2024-09-01 19:27:11 -0300

    Geração e Teste das Requisições HTTP via Thunder Client com armazenamento no MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,74 @@
 import express, { request, response } from "express"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
-
-app.post('/usuarios', (request, response) => {
+app.post('/usuarios', async (request, response) => {
     
-    users.push(request.body)
+    await prisma.usuario.create({
+        data: {
+            email: request.body.email,
+            nome: request.body.nome,
+            idade: request.body.idade
+        }
+    })
 
-    response.status(201).json(request.body)
+    response.status(201).json({
+        message: "Usuário CADASTRADO com SUCESSO!"
+    })
 })
 
-app.get('/usuarios', (request, response) => {
-    response.status(200).json(users)
+app.put('/usuarios/:id', async (request, response) => {
+    
+    await prisma.usuario.update({
+        where: {
+            id: request.params.id
+        },
+        data: {
+            email: request.body.email,
+            nome: request.body.nome,
+            idade: request.body.idade
+        }
+    })
+
+    response.status(200).json({
+        message: "Usuário ATUALIZADO com SUCESSO!"
+    })
+})
+
+app.delete('/usuarios/:id', async (request, response) => {
+
+    await prisma.usuario.delete({
+        where: {
+            id: request.params.id
+        }
+    })
+
+    response.status(200).json({
+        message: "Usuário DELETADO com SUCESSO!"
+    })
+})
+
+app.get('/usuarios', async (request, response) => {
+
+    let usuarios = []
+
+    if(request.query){
+        usuarios = await prisma.usuario.findMany({
+            where: {
+                email: request.query.email,
+                nome: request.query.nome,
+                idade: request.query.idade
+            }
+        })
+    } else {
+        usuarios = await prisma.usuario.findMany()
+    }
+    
+    response.status(200).json(usuarios)
 })
 
 app.listen(3000)
